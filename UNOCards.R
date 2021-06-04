c26210c844f109dc7ca9395c1cb7ea40e5a6ef29
Ant-28 <ananthajit5d@gmail.com>
2021-06-04 14:52:57 +0530

    added comments, I may never be able to understand them in the future but well I'll do it anyways hoping I may understand it.

--- UNOCards.R
+++ UNOCards.R
@@ -5,6 +5,7 @@ ifelse(lib_use %in% installed.packages(), require(lib_use,character.only =  TRUE
   library(lib_use, character.only = TRUE)
 })
 
+#used to create the deck of UNO cards
 {colours <- c("Red", "Blue", "Green", "Yellow")
 numbers <- c(0,rep(1:9,2))
 numbers <- as.character(numbers)
@@ -15,6 +16,7 @@ not_wilds_color <- expand.grid(colours, not_wilds)
 not_wilds_color <- str_c(not_wilds_color[,1], not_wilds_color[,2], sep = " ")}
 unocards <- c(not_wilds_color, wilds)
 
+#seed generation torture
 seedgen <- function()
 {
   #why do I do this?
@@ -24,29 +26,28 @@ seedgen <- function()
   sample(torturesample, size = 1)
 }
 
+#card sampling algorithm
 cardsample <- function(numplayers,deck,numcards){
  
   cards_drawn <- numplayers*numcards
-  #another layer of randomness because why not?
-  set.seed(seedgen())
+  
+  #Checks if more than 80 cards are drawn
   if(cards_drawn > 80){ 
-    deckerror()
+    "Discard Pile Too Small, There Should be at least 28 cards in the discard pile"
   } 
+  # The Actual card sampling algorithm
   else{
-  deckbuilder(deck, cards_drawn, numplayers)
+    #another layer of randomness because why not? 
+    # Added this within else because it made more sense to read
+    set.seed(seedgen())
+    # this thing
+    cardsample <- sample(deck, cards_drawn, replace = FALSE)
+    # proof the print function in R is horrible or, I'm stupid
+    cards <- split(cardsample,str_c('Player',c(1:numplayers), sep = " "))
+    cards
   }
 }
 
-
-deckbuilder <- function(deck, cards_drawn, numplayers){
-  
-  cardsample <- sample(deck, cards_drawn, replace = FALSE)
-  cards <- split(cardsample,str_c('Player',c(1:numplayers), sep = " "))
-  cards
-}
-deckerror <- function(){
-  "Discard Pile Too Small, There Should be at least 28 cards in the discard pile"
-}
-
+# Test out by tinkering arguments/output
 Deck <- cardsample(8,unocards,10)
 Deck$`Player 1`
